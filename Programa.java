a7430ce0134bc2f02deb56aa19733354115dcd4a
ivan <scorpoin32@gmail.com>
2021-06-01 23:21:56 -0500

    cambios en diseño

--- Programa.java
+++ Programa.java
@@ -75,22 +75,22 @@ public class Programa{
 
 public static void menu(){
 		  System.out.println("_____________________________________________________________________________ ");
-          System.out.println("|             _  __               _                        _                 |");
-          System.out.println("|           | |/ /              | |                      | |                 |"); 
-          System.out.println("|           | ' / __ _ _ __ ___ | | _____  __ _ _ __   __| | ___             |"); 
-          System.out.println("|           |  < / _` | '__/ _ \\| |/ / _ \\/ _` | '_ \\ / _` |/ _ \\        |"); 
-          System.out.println("|           | . \\ (_| | | | (_) |   <  __/ (_| | | | | (_| | (_) |           |"); 
-          System.out.println("|           |_|\\_\\__,_|_|  \\___/|_|\\_\\___|\\__,_|_| |_|\\__,_|\\___/    |"); 
+          System.out.println("|                                                                            |");
+          System.out.println("|                                                                            |"); 
+          System.out.println("|                                                                            |"); 
+          System.out.println("|                                                                            |"); 
+          System.out.println("|                                                                             |"); 
+          System.out.println("|                                                                             |"); 
           System.out.println("|                                                                             |");
           System.out.println("|                                                                             |"); 
 
-        System.out.println("|                                 /|                                           |");
-        System.out.println("|                   =  =  =      / |                                           |");
-        System.out.println("|              ____| || || |____/  | -_-_-_-_-_-_                             |");
-        System.out.println("|            |)----| || || |____   |     AH                                    |");
-        System.out.println("|              ((  | || || |  ))\\  | _-_-_-_-_-_-                             |");
-        System.out.println("|               \\\\_|_||_||_|_//  \\ |                                        |");
-        System.out.println("|                \\___________/    \\|                                         |");
+        System.out.println("|                                                                              |");
+        System.out.println("|                                                                              |");
+        System.out.println("|                                                                             |");
+        System.out.println("|                                                                              |");
+        System.out.println("|                                                                              |");
+        System.out.println("|                                                                              |");
+        System.out.println("|                                                                              |");
 
  
 
